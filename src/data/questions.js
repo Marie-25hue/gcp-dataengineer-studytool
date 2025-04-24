2acec5f35e666bf6c0f7735a4e5b1a44068bf9d2
Mariela Rajoy <marielaflaviarajoy@gmail.com>
2025-04-23 22:30:08 -0300

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,1 +1,52 @@
-
+[
+  {
+    "question": "¿Cuál es la función principal de BigQuery?",
+    "options": [
+      "Almacenamiento de datos",
+      "Machine learning",
+      "Routing de red",
+      "Contenedores"
+    ],
+    "answer": "Almacenamiento de datos"
+  },
+  {
+    "question": "¿Qué servicio GCP se usa para flujos de datos en tiempo real?",
+    "options": [
+      "Cloud Pub/Sub",
+      "Cloud Storage",
+      "BigQuery",
+      "Cloud Build"
+    ],
+    "answer": "Cloud Pub/Sub"
+  },
+  {
+    "question": "¿Qué comando de gcloud lista tus proyectos?",
+    "options": [
+      "gcloud auth list",
+      "gcloud projects list",
+      "gcloud config list",
+      "gcloud info"
+    ],
+    "answer": "gcloud projects list"
+  },
+  {
+    "question": "¿Qué clase de almacenamiento es ideal para archivos poco accedidos?",
+    "options": [
+      "Standard",
+      "Nearline",
+      "Coldline",
+      "Multi-Regional"
+    ],
+    "answer": "Coldline"
+  },
+  {
+    "question": "¿Qué significa IAM en GCP?",
+    "options": [
+      "Identity and Access Management",
+      "Infrastructure Access Module",
+      "Internal Analytics Model",
+      "Instant App Monitor"
+    ],
+    "answer": "Identity and Access Management"
+  }
+]
