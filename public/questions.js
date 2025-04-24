c8957cd5e0cffaa6361ea6accec03918508c8d66
Mariela Rajoy <marielaflaviarajoy@gmail.com>
2025-04-24 10:58:19 -0300

    Create questions.js

---
+++ public/questions.js
@@ -0,0 +1,12 @@
+[
+  {
+    "question": "¿Qué es una base de datos relacional?",
+    "options": ["Conjunto de tablas relacionadas", "Archivo de texto plano", "Una red social", "Un servidor web"],
+    "answer": "Conjunto de tablas relacionadas"
+  },
+  {
+    "question": "¿Qué hace un SELECT en SQL?",
+    "options": ["Selecciona datos", "Borra datos", "Actualiza datos", "Inserta datos"],
+    "answer": "Selecciona datos"
+  }
+]
